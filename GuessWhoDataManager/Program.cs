387805d1fcdd8d9c1a88fadcfb7997087089d1d9
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2021-08-17 16:32:08 +0200

    Removed unused test code.

--- GuessWhoDataManager/Program.cs
+++ GuessWhoDataManager/Program.cs
@@ -1,20 +1,11 @@
 ﻿using System;
-using System.Linq;
-using System.Xml;
 
 namespace GuessWhoDataManager {
     internal class Program {
-        static void Main(string[] args) {
+        static void Main() {
             // todo: add args etc., I guess
             DataManager dataManager = new DataManager();
-            //dataManager.ReworkResources(true);
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load("D:\\VisualStudioRepos\\GuessWho\\GuessWhoResources\\GuessWhoResources.csproj");
-            XmlNodeList resources = doc.GetElementsByTagName("Resource");
-            foreach (XmlNode node in resources.Cast<XmlNode>()) {
-                var af = node.Attributes["Include"].Value;
-            }
+            dataManager.ReworkResources(true);
             Console.ReadKey();
         }
     }
